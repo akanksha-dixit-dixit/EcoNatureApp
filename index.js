4cc343654b02e20c77f72b584a58f6e9e78dd71f
Akanksha <shilpidixit280@gmail.com>
2024-02-22 23:55:12 +0530

    implemented weather

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-import { sum, userAction } from './utility.js';
+import * as helper from './utility.js';
 
 //Weather Icon Functionality
 
-const toolBar = document.querySelector('.ToolBar');
-const weatherDetails = document.querySelector('.weatherDetails');
-const headerIcons = document.querySelector('.header-icons');
+// const toolBar = document.querySelector('.ToolBar');
+// const weatherDetails = document.querySelector('.weatherDetails');
+// const headerIcons = document.querySelector('.header-icons');
 
 // Api hit  on windowonload
 
 window.addEventListener('load', async (event) => {
-  userAction();
+  helper.getWeather();
 });
+
+//getter for weather icon
+
+const detailsOfweather = document.querySelector('.detailsOfweather');
+const detailsOfLocation = document.querySelector('.detailsOfLocation');
+const rotateIcons = document.querySelector('#rotateArrow');
+
+rotateIcons.addEventListener('click', () => {
+  helper.getWeather();
+});
+
+
+
--- utility.js
+++ utility.js
@@ -3,27 +3,28 @@ export const sum = (a, b) => {
 };
 
 //Fetching weather
+const detailsOfweather = document.querySelector('.detailsOfweather');
+const detailsOfLocation = document.querySelector('.detailsOfLocation');
+const rotateIcons = document.querySelector('#rotateArrow');
 const APIKey = '34ea5b767fd0a6b704c9b976a2b57631';
 const APIUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
-export const userAction = async (lat, lon) => {
+
+const fetchWeather = async (lat, lon) => {
   const response = await fetch(
-    `${APIUrl}&appid=${APIKey}?lat=${lat}?lon=${lon}`
+    `${APIUrl}?appid=${APIKey}&lat=${lat}&lon=${lon}&units=metric`
   );
-  const myJson = await response.json(); //extract JSON from the http response
+  const data= await response.json();
+  // console.log(rest);
+  detailsOfLocation.innerHTML = data.name;
+  detailsOfweather.innerHTML =  data.main.temp;
 };
 
-const error = (err) => console.warn(`ERROR(${err.code}): ${err.message}`);
-const success = (pos) => {
-  const crd = pos.coords;
-  const lat = crd.latitude;
-  const lon = crd.longitude;
+export const getWeather = () => {
+  navigator.geolocation.getCurrentPosition((pos) => {
+    const lat = pos.coords.latitude;
+    const lon = pos.coords.longitude;
 
-  userAction(lat, lon);
+    fetchWeather(lat, lon);
+  });
 };
-const options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0,
-};
-navigator.geolocation.getCurrentPosition(success, error, options);
