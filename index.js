c308442644a57cea12abe22b7c3be33a92fa851f
Akanksha <shilpidixit280@gmail.com>
2024-03-02 19:52:45 +0530

    completed navbar, initialized banner

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
-import { sum, userAction } from './utility.js';
+import * as helper from './utility.js';
 
 //Weather Icon Functionality
 
-const toolBar = document.querySelector('.ToolBar');
-const weatherDetails = document.querySelector('.weatherDetails');
-const headerIcons = document.querySelector('.header-icons');
+// const toolBar = document.querySelector('.ToolBar');
+// const weatherDetails = document.querySelector('.weatherDetails');
+// const headerIcons = document.querySelector('.header-icons');
 
 // Api hit  on windowonload
 
-window.addEventListener('load', async (event) => {
-  userAction();
+window.addEventListener('load', (event) => {
+  helper.getWeather();
+});
+
+//getter for weather icon
+
+const detailsOfweather = document.querySelector('.detailsOfweather');
+const detailsOfLocation = document.querySelector('.detailsOfLocation');
+const rotateIcons = document.querySelector('#rotateArrow');
+
+rotateIcons.addEventListener('click', () => {
+  helper.getWeather();
+});
+
+//getter for refresh icon
+
+let rotateArrow = document.querySelector('#rotateArrow');
+rotateArrow.addEventListener('click', () => {
+  rotateArrow.classList.toggle('refresh');
+});
+
+//getter for theme mode
+let body = document.body;
+let theme = document.querySelector('.theme');
+theme.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
+
+let day = document.querySelector('#day');
+let night = document.querySelector('#night');
+
+day.addEventListener('click', () => {
+
+  night.classList.add('show');
+  day.classList.remove('show');
+  day.classList.add('hide')
+  night.classList.remove('hide')
+
+
+  // day.style.display = 'none'
+  // night.style.display = 'block';
+});
+night.addEventListener('click', () => {
+  night.classList.add('hide');
+  day.classList.remove('hide');
+  day.classList.add('show')
+  night.classList.remove('show')
+  //  night.style.display = 'none'
+  //   day.style.display ='block';
 });
