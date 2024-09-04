657550d9b2f67c187a3ba78b6811c53594d505b8
akanksha-dixit-dixit <shilpidixit280@gmail.com>
2024-09-04 22:08:02 +0530

    testing changes

--- index.js
+++ index.js
@@ -322,27 +322,27 @@ Theme1.addEventListener('click', () => {
 //  })
 // i8n js libary
 //i18n
-i18next.init({
-  lng: 'en', // if you're using a language detector, do not define the lng option
-  debug: true,
-  resources: {
-    en: {
-      translation: {
-        key: 'Our Partners',
-      },
-    },
-    sp: {
-      translation: {
-        key: 'El francias',
-      },
-    },
-  },
-});
+// i18next.init({
+//   lng: 'en', // if you're using a language detector, do not define the lng option
+//   debug: true,
+//   resources: {
+//     en: {
+//       translation: {
+//         key: 'Our Partners',
+//       },
+//     },
+//     sp: {
+//       translation: {
+//         key: 'El francias',
+//       },
+//     },
+//   },
+// });
 // initialized and ready to go!
 // i18next is already initialized, because the translation resources where passed via init function
-const temp = document.getElementById('temp');
-document.getElementById('output').innerHTML = i18next.t('key');
-temp.addEventListener('click', (e) => {
-  i18next.changeLanguage('sp');
-  document.getElementById('output').innerHTML = i18next.t('key');
-});
+// const temp = document.getElementById('temp');
+// document.getElementById('output').innerHTML = i18next.t('key');
+// temp.addEventListener('click', (e) => {
+//   i18next.changeLanguage('sp');
+//   document.getElementById('output').innerHTML = i18next.t('key');
+// });
